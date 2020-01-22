e96d596353a245db87d8ccc3dd94b9abc77f76dd
iambashar <bashar38@iut-dhaka.edu>
2020-01-22 18:16:43 +0600

    Modify the readme

---
+++ Again_Again_WS.py
@@ -0,0 +1,79 @@
+import time
+p = 1
+
+while p < 6:
+    f = open("log.md", "a+")
+    from requests import Session
+    from bs4 import BeautifulSoup as bs
+    import re
+
+    i = 1
+
+    lines = "Hi"
+
+
+    file = open("idpass.txt", "r")
+    lines = file.read()
+    dpass = '1234'
+    dtime = 5800
+    
+
+    l = lines.split(" ")
+
+    while i:
+        try:
+            un = l[(2*i)-2]
+            ps = l[(2*i)-1]
+            #print(un, ps)
+            with Session() as s:    
+                site = s.get("http://10.220.20.12/index.php/home/loginProcess")
+                bs_content = bs(site.content, "html.parser")
+                login_data = {"username":un,"password":ps}
+                s.post("http://10.220.20.12/index.php/home/loginProcess",login_data)
+                home_page = s.get("http://10.220.20.12/index.php/home/dashboard")
+                soup = bs(home_page.content, "lxml")
+
+                #print(results)
+
+                table = soup.table
+
+                c = 1
+                li = []
+
+
+                try:
+                    table_rows = table.find_all('tr')
+                    for tr in table_rows:
+                        td = tr.find_all('td')
+                        row = [i.text for i in td]
+                        if c == 2 or c == 6 :
+                            li.append(row[1])
+                        c += 1
+                    print(li[0])
+                    L = ["\n", li[0], li[1]]
+                    f.writelines(L)
+                    string1 = li[1]
+                    m = int(re.search(r'\d+', string1).group())
+                    print(m, 'Minutes')
+                    #f.write(" %d Minutes\n" % m)
+                    #check(m)
+                    if m > dtime:
+                        site = s.get("http://10.220.20.12/index.php/home/chPasswordProcess")
+                        bs_content = bs(site.content, "html.parser")
+                        cpass_data = {"curr_pass": ps, "new_pass": dpass, "conf_pass": dpass}
+                        s.post("http://10.220.20.12/index.php/home/chPasswordProcess", cpass_data)
+                        #print("Your Password is set to", dpass)
+                        f.write("Your Password is set to %s" % dpass)
+
+                except:
+                    f.write("The username/password you entered is incorrect.\nTry again...")
+                    #print("The username/password you entered is incorrect.\nTry again...")
+        except:
+            break
+        i += 1
+    p += 1
+    f.close()
+    time.sleep(300)
+
+file.close()
+#t = input("Press enter to terminate")
---
+++ README.md
@@ -0,0 +1,41 @@
+#IUSERSCHECK
+
+Hello good people.
+
+The Project will help you a lot, if you are an IUTIAN & most importantly Residential Student.
+
+Do you ever lose your pocket allowance for the overtime bill of IUT's Internet Service?
+If not, then you are lucky, but most people often have to face this situation.
+The situation is even more painful if one of your roommates' minutes are free, but your
+pocket allowance ends up deducted.
+
+So I came up with a solution for this problem.
+
+What if, you don't check your usage regularly yourself, but let a silly script do that for you?
+This is what I made for you guys. This python script will do this task for you.
+
+##Usage
+
+Just open the "idpass.txt" file & enter your four or more(:v) roommates userid & password & save it
+Add "tool.vbs" to startup & restart your computer, That's all. Checking the log will give you more info
+about your usage.
+
+
+
+You have to do only 3 tasks & Forget about everything else.
+
+1. Download the git in zip format & unzip it & store in a permanent folder
+2. Open the "idpass.txt", in this file store your userid & password in the format shown 
+(At first delete the line give as an example) & close it after save
+3. Press "Windows+R" type `shell:startup` Create a "New Shortcut", give the path of `tool.vbs` from your folder, 
+Click next next When all is ok, Then restart your PC, & forget about to check your usage.
+
+Attention: When your usage will cross the limit 5800 minutes then your IUSERS password will be automatically changed to `1234` so that your router can't connect to internet
+by your password. So when you see that your router can't connect to internet then you are assuered that you pass the limit of 5800 minutes & also your
+password is now `1234`
+
+Also, as of right now, this script is still a WIP (and therefore very unpolished, so expect many changes to the script in the near future. (GUI coming soon xD)
+
+If you like the project please keep supporting me.
+M. K. Bashar
+180041238
