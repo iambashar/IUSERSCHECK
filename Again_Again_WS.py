daa701f43f7bcce1eb4830e4d3f0665719eff2bf
M. K. Bashar <bashar.onion@gmail.com>
2022-02-25 00:42:23 +0600

    Implement router cli functionality

--- Again_Again_WS.py
+++ Again_Again_WS.py
@@ -1,77 +1,131 @@
-import time
+import telnetlib
+from time import sleep
+from requests import Session
+from bs4 import BeautifulSoup as bs
+import re
+import csv
 
-while True:
-    f = open("log.md", "a+")
-    from requests import Session
-    from bs4 import BeautifulSoup as bs
-    import re
+current_id = 0
+flag = False
+current_user = ''
 
-    i = 1
+HOST = '192.168.0.1'
+router_username = b'admin'
+router_password = b'admin'
 
-    lines = "Hi"
+with telnetlib.Telnet(HOST) as tn:
+    tn.read_until(b"username:", 2)
+    tn.write(router_username + b'\n')
+    tn.write(router_password + b'\n')
 
+    sleep(2)
+    tn.write(b'wan show connection info\n')
 
-    file = open("idpass.txt", "r")
-    lines = file.read()
-    dpass = '1234'
-    dtime = 5800
+    tn.read_until(b'username=')
     
+    current_user =  tn.read_very_eager().decode('ascii').splitlines()[0]
+    
+    sleep(2)
+    tn.write(b'logout\n')
 
-    l = lines.split(" ")
+f_idpass = open("idpass.txt", "r")
+c_idpass = list(csv.reader(f_idpass))
 
-    while i:
+c = 0 
+for idpass in c_idpass:
+
+    un = idpass[0].strip()
+
+    if un == current_user :
+        current_id = c
+        break
+    c += 1
+
+# runs every 5 mins
+while True:
+    f_idpass = open("idpass.txt", "r")
+
+    router_username = b'admin'
+    router_password = b'admin'
+
+    HOST = '192.168.0.1'
+
+    minute_thresh = 5750
+    c_usages = []
+
+    c_idpass = list(csv.reader(f_idpass))
+    
+    minute_limit = 0
+
+    for idpass in c_idpass:
+
         try:
-            un = l[(2*i)-2]
-            ps = l[(2*i)-1]
-            #print(un, ps)
-            with Session() as s:    
+            un = idpass[0].strip()
+            pw = idpass[1].strip()
+
+            # un = l[(2*i)-2]
+            # ps = l[(2*i)-1]
+            
+            with Session() as s:
                 site = s.get("http://10.220.20.12/index.php/home/loginProcess")
                 bs_content = bs(site.content, "html.parser")
-                login_data = {"username":un,"password":ps}
+                login_data = {"username":un,"password":pw}
                 s.post("http://10.220.20.12/index.php/home/loginProcess",login_data)
                 home_page = s.get("http://10.220.20.12/index.php/home/dashboard")
                 soup = bs(home_page.content, "lxml")
-
-                #print(results)
 
                 table = soup.table
 
                 c = 1
                 li = []
 
-
                 try:
                     table_rows = table.find_all('tr')
                     for tr in table_rows:
                         td = tr.find_all('td')
                         row = [i.text for i in td]
-                        if c == 2 or c == 6 :
+                        if c == 2 or c == 6 or c == 5 :
                             li.append(row[1])
                         c += 1
-                    print(li[0])
-                    L = ["\n", li[0], li[1]]
-                    f.writelines(L)
-                    string1 = li[1]
-                    m = int(re.search(r'\d+', string1).group())
-                    print(m, 'Minutes')
-                    #f.write(" %d Minutes\n" % m)
-                    #check(m)
-                    if m > dtime:
-                        site = s.get("http://10.220.20.12/index.php/home/chPasswordProcess")
-                        bs_content = bs(site.content, "html.parser")
-                        cpass_data = {"curr_pass": ps, "new_pass": dpass, "conf_pass": dpass}
-                        s.post("http://10.220.20.12/index.php/home/chPasswordProcess", cpass_data)
-                        #print("Your Password is set to", dpass)
-                        f.write("Your Password is set to %s" % dpass)
 
-                except:
-                    f.write("The username/password you entered is incorrect.\nTry again...")
-                    #print("The username/password you entered is incorrect.\nTry again...")
+                    # update list and sort
+                    string1 = li[2]
+                    string2 = li[1]
+                    minute_limit = int(re.search(r'\d+', string2).group())   # limit
+                    minute_used = int(re.search(r'\d+', string1).group())    # used minutes
+                    print(f'{un}\t\t{minute_used}')
+
+                    minute_thresh = int(.96 * float(minute_limit))
+                    c_usages.append([un, pw, minute_limit - minute_used])
+                    
+                except Exception as e:
+                    print (e)
         except:
-            break
-        i += 1
-    f.close()
-    time.sleep(300)
+            continue
+    
+    print (current_id)
+    if int(minute_limit - c_usages[current_id][2]) > minute_thresh:
+        current_id = (current_id + 1) % len(c_usages)
+        flag = True
+        
+    else :
+        if flag == True :
+            username = c_usages[current_id][0]
+            password = c_usages[current_id][1]
+            print (username)
 
-file.close()
+            with telnetlib.Telnet(HOST) as tn:
+                tn.read_until(b"username:", 2)
+                tn.write(router_username + b'\n')
+                tn.write(router_password + b'\n')
+
+                sleep(2)
+                tn.write(b'wan set service ewan_pppoe --protocol pppoe --username ' + username.encode('ascii') + b' --password  ' + password.encode('ascii') + b' --secondConnection sec_conn_dynip\n')
+                
+                sleep(2)
+                tn.write(b'logout\n')
+                flag  = False         
+        
+    sleep(300)
+
 #t = input("Press enter to terminate")
